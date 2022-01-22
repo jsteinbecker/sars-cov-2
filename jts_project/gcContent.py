fcab821a7a54ff0ace31dc6e91d97600ebb62b6f
jsteinbecker <94640608+jsteinbecker@users.noreply.github.com>
2022-01-21 19:12:48 -0700

    Update gcContent.py

--- jts_project/gcContent.py
+++ jts_project/gcContent.py
@@ -1,5 +1,10 @@
 def gcContent (seq):
+   """
+   Function: Return GC Content of Nucleic Acid Sequence. Returned as decimal value. 
+   """
+
    seq = seq.lower()
    gcPerc =round((seq.count("g")+seq.count("c")) / len(seq),4)
+
    return gcPerc
 
