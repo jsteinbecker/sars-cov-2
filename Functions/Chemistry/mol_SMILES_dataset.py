e836f5b54b2b6c95f51f9f71775b64b67361470f
jsteinbecker <joshsteinbecker@icloud.com>
2022-01-22 21:13:56 -0700

    data added

--- Functions/Chemistry/mol_SMILES_dataset.py
+++ Functions/Chemistry/mol_SMILES_dataset.py
@@ -1,10 +1,16 @@
-
-nicodozole = "COC(=O)NC1=NC2=CC=C(C(=O)C3=CC=CS3)C=C2N1"
-benzene = "c1ccccc1"
+col = {
+   "nicodozole" : "COC(=O)NC1=NC2=CC=C(C(=O)C3=CC=CS3)C=C2N1",
+   "benzene" : "c1ccccc1",
+   "glycine" : "NCC(=O)O",
+   "tyrosine" : "NC(c1ccc(O)cc1)C(=O)O",
+   "alanine" : "NC(C)C(=O)O",
+   "GWA" : "NCC(=O)ONC(c1ccc(O)cc1)C(=O)ONC(C)C(=O)O"
+}
 
 from rdkit import Chem
 from rdkit.Chem.rdmolfiles import MolFromSmiles
 from rdkit.Chem import Draw
 
-n = Chem.MolFromSmiles(benzene)
-Chem.Draw.MolsToImage(n)
+
+n = MolFromSmiles(col["glycine"])
+Draw.MolToImage(n).show()
