058168afdd9ddd55588f1c430b478eb93d8b0176
jsteinbecker <joshsteinbecker@icloud.com>
2022-01-22 22:03:43 -0700

    edit

---
+++ Functions/Chemistry/vmol.py
@@ -0,0 +1,17 @@
+
+from rdkit import Chem
+from rdkit.Chem.rdmolfiles import MolFromSmiles
+from rdkit.Chem import Draw
+
+import pandas as pd
+
+file = pd.read_csv("/Users/joshsteinbecker/jts_project/Functions/Chemistry/smilesData.csv")
+data = pd.DataFrame(file)
+
+def vmol (mol_name):
+   smiles = data.loc[data['MolName'] == mol_name, 'SMILES'].item()
+   mol = MolFromSmiles(smiles)
+   return Draw.MolToImage(mol).show() 
+
+
+vmol("isopropanol")
