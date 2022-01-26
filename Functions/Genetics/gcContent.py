62d9dd48e20ae219cfd341fda0a6173ab4cae50e
Josh Steinbecker <joshsteinbecker@icloud.com>
2022-01-26 08:18:44 -0700

    edit

--- Functions/Genetics/gcContent.py
+++ Functions/Genetics/gcContent.py
@@ -1,10 +1,14 @@
 def gcContent (seq):
    """
    Function: Return GC Content of Nucleic Acid Sequence. Returned as decimal value. 
+
    """
+   
    seq = seq.lower()
 
    gcPerc = round((seq.count("g")+seq.count("c")) / len(seq),4)
 
    return gcPerc
 
+seq='accgtgacgtgtgacgtgccgtag'
+print(gcContent(seq))
--- Functions/Math/roundUptoMultiple.py
+++ Functions/Math/roundUptoMultiple.py
@@ -1,3 +1,4 @@
 def roundUpToMultiple(number, multiple):
     num = number + (multiple - 1)
+    
     return num - (num % multiple)
