05d5fd76e293c8dd0b2619db75ec9afde0a1652e
jsteinbecker <94640608+jsteinbecker@users.noreply.github.com>
2022-02-08 07:54:19 -0700

    Update cleanDNAstring.py

--- Functions/Genetics/cleanDNAstring.py
+++ Functions/Genetics/cleanDNAstring.py
@@ -1,5 +1,14 @@
 import re
 
+"""
+#############
+CLEAN DNA STRING
+Function
+-------------
+REMOVES DIGITS SPACES AND NEWLINE CHARACTERS FROM INPUT
+#############
+"""
+
 def cleanDNA (seq:str):
    seq = re.sub("\d","",seq)
    seq = re.sub("\s","",seq)
