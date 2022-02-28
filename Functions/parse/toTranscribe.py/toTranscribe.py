4888d28867e1db3e67b550b6c70132431dd4dfda
jsteinbecker <joshsteinbecker@icloud.com>
2022-02-28 14:20:45 +0000

    edit

--- Functions/parse/toTranscribe.py/toTranscribe.py
+++ Functions/parse/toTranscribe.py/toTranscribe.py
@@ -9,13 +9,13 @@ data = """
 Rogue One:
     composer : GIACCHINO
     movements : 
-        - Good Luck Little Sister : clarinet
-        - Your Father Would Be Proud : cello
+        - Good Luck Little Sister *CT
+        - Your Father Would Be Proud *CO
 
 HTTYD- The Hidden World :
     composer : POWELL
     movements : 
-        - With Love Comes a Great Waterfall : violin
+        - With Love Comes a Great Waterfall *VN
 """
 
 
@@ -26,7 +26,21 @@ df = pd.DataFrame(y)
 df = df.T
 df
 # %%
-df.explode('movements')
+df = df.explode('movements')
+df
 # %%
 list_of_composers = list(df.composer.unique())
 list_of_composers
+
+# %%
+import re
+# %%
+df['movements'] = df['movements'].astype("str")
+# %%
+df['inst'] = df['movements'].str.extract("(\*\w\w)")
+df['movements'] = df['movements'].str.replace("(\*\w\w)","",regex=True)
+# %%
+df
+# %%
+df.pivot(columns="composer",index="movements")
+# %%
