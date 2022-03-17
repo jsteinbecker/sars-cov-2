b613cc90a0c33da91b5f5875e0a69a9751b9cf0d
jsteinbecker <joshsteinbecker@icloud.com>
2022-03-17 06:57:16 -0600

    edit

--- merm.py
+++ merm.py
@@ -3,7 +3,9 @@ import base64
 from IPython.display import Image, display
 import matplotlib.pyplot as plt
 
-def mm(graph):
+def mm(graph,darkmode=1):
+  if darkmode == 1:
+    graph = "%%{init: {'theme': 'dark'}}%%" + graph
   graphbytes = graph.encode("ascii")
   base64_bytes = base64.b64encode(graphbytes)
   base64_string = base64_bytes.decode("ascii")
@@ -16,7 +18,7 @@ graph LR;
     C--> A & E;
     D--> A & E;
     E--> B & C & D;
-""")
+""",1)
 
 # %%
 g = """
@@ -25,16 +27,15 @@ graph TD;
    R((Runner)) --> 7C & MI & EP & 3
    PD((PDesk)) --> 7P & EP & N
 """
-mm(g)
+mm(g,1)
 
 # %%
 g = """
-%%{init: {'theme': 'dark'}%%
         graph TD
-          A[fa:fa-tree Christmas] -.->| fa:fa-money Get money .| B(Go shopping)
-          B -.-> C{fa:fa-cloud <br> Let me think...}
+          A[fa:fa-tree Christmas] -.->| x fa:fa-money Get money .| B(Go shopping)
+          B -.-> C{fa:fa-cloud <br> Let me<br>think...}
           C -.-> B
-          B --> G[/fa:fa-gemini Another/]
+          B --> G[/fa:fa-draft2digital Another/]
           C ==>|One| D[fa:fa-laptop Laptop_ ]
           C -->|Two| E[fa:fa-mobile iPhone_ ]
           C -.->|Three| F(fa:fa-car Car_)
@@ -46,17 +47,18 @@ g = """
             G
           end"""
 mm(g)
-
-# %%
-%%HTML
-<p>hjk
-<a href="{% static 'fontawesomefree/css/all.min.css' %}" rel="stylesheet" type="text/css"></p>
-
-
 # %%
 g = """
-graph TD;
-  genwk --calcs--> genrow
+graph LR;
+  subgraph DATA
+    Week((Week<br>fa:fa-calendar)) --> Cols
+    Employees((Empl<br>fa:fa-user)) --> Rows
+    Cols --> DataFrame(DataFrame<br>fa:fa-database)
+    Rows --> DataFrame
+  end
+  subgraph ANALYSIS
+    DataFrame --> Stars{fa:fa-star<br>Stars}
+  end
 """
 mm(g)
 # %%
